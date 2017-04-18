ad95c1f8a899bbb941edefe4908d1b2f31aaffaa
YggDrazil <yggdrazil@hotmail.de>
2017-04-18 07:00:36 +0200

    Update default.py

--- default.py
+++ default.py
@@ -1,92 +1,203 @@
-# https://docs.python.org/2.7/
-import os
+# -*- coding: utf-8 -*-
+# Module: default
+# Author: Roman V. M.
+# Created on: 28.11.2014
+# License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
+
 import sys
-import urllib
-import urlparse
-# http://mirrors.kodi.tv/docs/python-docs/
-import xbmcaddon
+from urllib import urlencode
+from urlparse import parse_qsl
 import xbmcgui
 import xbmcplugin
-# http://docs.python-requests.org/en/latest/
-import requests
-# http://www.crummy.com/software/BeautifulSoup/bs4/doc/
-from bs4 import BeautifulSoup
 
-def build_url(query):
-    base_url = sys.argv[0]
-    return base_url + '?' + urllib.urlencode(query)
-    
-def get_page(url):
-    # download the source HTML for the page using requests
-    # and parse the page using BeautifulSoup
-    return BeautifulSoup(requests.get(url).text, 'html.parser')
-    
-def parse_page(page):
-    songs = {}
-    index = 1
-    # the sample below is specific for the page we are scraping
-    # you will need to view the source of the page(s) you are
-    # planning to scrape to find the content you want to display
-    # this will return all the <a> elements on the page:
-    # <a href="some_url">some_text</a>
-    for item in page.find_all('a'):
-        # the item contains a link to an album cover
-        if item['href'].find('.jpg') > 1:
-            # format the url for the album cover to include the site url and url encode any spaces
-            album_cover = '{0}{1}'.format(sample_page, item['href'].replace(' ', '%20'))
-        # the item contains a link to a song containing '.mp3'
-        if item['href'].find('.mp3') > 1:
-            # update dictionary with the album cover url, song filename, and song url
-            songs.update({index: {'album_cover': album_cover, 'title': item['href'], 'url': '{0}{1}'.format(sample_page, item['href'])}})
-            index += 1
-    return songs
-    
-def build_song_list(songs):
-    song_list = []
-    # iterate over the contents of the dictionary songs to build the list
-    for song in songs:
-        # create a list item using the song filename for the label
-        li = xbmcgui.ListItem(label=songs[song]['title'], thumbnailImage=songs[song]['album_cover'])
-        # set the fanart to the albumc cover
-        li.setProperty('fanart_image', songs[song]['album_cover'])
-        # set the list item to playable
-        li.setProperty('IsPlayable', 'true')
-        # build the plugin url for Kodi
-        # Example: plugin://plugin.audio.example/?url=http%3A%2F%2Fwww.theaudiodb.com%2Ftestfiles%2F01-pablo_perez-your_ad_here.mp3&mode=stream&title=01-pablo_perez-your_ad_here.mp3
-        url = build_url({'mode': 'stream', 'url': songs[song]['url'], 'title': songs[song]['title']})
-        # add the current list item to a list
-        song_list.append((url, li, False))
-    # add list to Kodi per Martijn
-    # http://forum.kodi.tv/showthread.php?tid=209948&pid=2094170#pid2094170
-    xbmcplugin.addDirectoryItems(addon_handle, song_list, len(song_list))
-    # set the content of the directory
-    xbmcplugin.setContent(addon_handle, 'songs')
-    xbmcplugin.endOfDirectory(addon_handle)
-    
-def play_song(url):
-    # set the path of the song to a list item
-    play_item = xbmcgui.ListItem(path=url)
-    # the list item is ready to be played by Kodi
-    xbmcplugin.setResolvedUrl(addon_handle, True, listitem=play_item)
-    
-def main():
-    args = urlparse.parse_qs(sys.argv[2][1:])
-    mode = args.get('mode', None)
-    
-    # initial launch of add-on
-    if mode is None:
-        # get the HTML for http://www.theaudiodb.com/testfiles/
-        page = get_page(sample_page)
-        # get the content needed from the page
-        content = parse_page(page)
-        # display the list of songs in Kodi
-        build_song_list(content)
-    # a song from the list has been selected
-    elif mode[0] == 'stream':
-        # pass the url of the song to play_song
-        play_song(args['url'][0])
-    
+# Get the plugin url in plugin:// notation.
+_url = sys.argv[0]
+# Get the plugin handle as an integer number.
+_handle = int(sys.argv[1])
+
+# Free sample videos are provided by www.vidsplay.com
+# Here we use a fixed set of properties simply for demonstrating purposes
+# In a "real life" plugin you will need to get info and links to video files/streams
+# from some web-site or online service.
+VIDEOS = {'Folge 001': [{'name': 'Die Jagd nach den Millionendieben,
+                       'thumb': 'http://kodinerds.bplaced.net/audiobooks/tkkg/img/001_cover.jpg',
+                       'video': 'http://kodinerds.bplaced.net/audiobooks/tkkg/TKKG%20-%20001%20-%20Die%20Jagd%20nach%20den%20Millonendieben.pls',
+                       'genre': 'Full'}
+                      ],
+           'Folge 002': [{'name': 'Mirror #1',
+                      'thumb': 'http://kodinerds.bplaced.net/audiobooks/tkkg/img/001_cover.jpg',
+                      'video': 'http://kodinerds.bplaced.net/audiobooks/tkkg/TKKG%20-%20001%20-%20Die%20Jagd%20nach%20den%20Millonendieben.pls',
+                      'genre': 'Full'}
+                     ],
+            'Folge 003': [{'name': 'Mirror #1',
+                      'thumb': 'http://kodinerds.bplaced.net/audiobooks/tkkg/img/001_cover.jpg',
+                      'video': 'http://kodinerds.bplaced.net/audiobooks/tkkg/TKKG%20-%20001%20-%20Die%20Jagd%20nach%20den%20Millonendieben.pls',
+                      'genre': 'TKKG'}
+                     ]}
+
+
+def get_url(**kwargs):
+    """
+    Create a URL for calling the plugin recursively from the given set of keyword arguments.
+
+    :param kwargs: "argument=value" pairs
+    :type kwargs: dict
+    :return: plugin call URL
+    :rtype: str
+    """
+    return '{0}?{1}'.format(_url, urlencode(kwargs))
+
+
+def get_categories():
+    """
+    Get the list of video categories.
+
+    Here you can insert some parsing code that retrieves
+    the list of video categories (e.g. 'Movies', 'TV-shows', 'Documentaries' etc.)
+    from some site or server.
+
+    .. note:: Consider using `generator functions <https://wiki.python.org/moin/Generators>`_
+        instead of returning lists.
+
+    :return: The list of video categories
+    :rtype: list
+    """
+    return VIDEOS.iterkeys()
+
+
+def get_videos(category):
+    """
+    Get the list of videofiles/streams.
+
+    Here you can insert some parsing code that retrieves
+    the list of video streams in the given category from some site or server.
+
+    .. note:: Consider using `generators functions <https://wiki.python.org/moin/Generators>`_
+        instead of returning lists.
+
+    :param category: Category name
+    :type category: str
+    :return: the list of videos in the category
+    :rtype: list
+    """
+    return VIDEOS[category]
+
+
+def list_categories():
+    """
+    Create the list of video categories in the Kodi interface.
+    """
+    # Get video categories
+    categories = get_categories()
+    # Iterate through categories
+    for category in categories:
+        # Create a list item with a text label and a thumbnail image.
+        list_item = xbmcgui.ListItem(label=category)
+        # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
+        # Here we use the same image for all items for simplicity's sake.
+        # In a real-life plugin you need to set each image accordingly.
+        list_item.setArt({'thumb': VIDEOS[category][0]['thumb'],
+                          'icon': VIDEOS[category][0]['thumb'],
+                          'fanart': VIDEOS[category][0]['thumb']})
+        # Set additional info for the list item.
+        # Here we use a category name for both properties for for simplicity's sake.
+        # setInfo allows to set various information for an item.
+        # For available properties see the following link:
+        # http://mirrors.xbmc.org/docs/python-docs/15.x-isengard/xbmcgui.html#ListItem-setInfo
+        list_item.setInfo('video', {'title': category, 'genre': category})
+        # Create a URL for a plugin recursive call.
+        # Example: plugin://plugin.video.example/?action=listing&category=Animals
+        url = get_url(action='listing', category=category)
+        # is_folder = True means that this item opens a sub-list of lower level items.
+        is_folder = True
+        # Add our item to the Kodi virtual folder listing.
+        xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
+    xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+    # Finish creating a virtual folder.
+    xbmcplugin.endOfDirectory(_handle)
+
+
+def list_videos(category):
+    """
+    Create the list of playable videos in the Kodi interface.
+
+    :param category: Category name
+    :type category: str
+    """
+    # Get the list of videos in the category.
+    videos = get_videos(category)
+    # Iterate through videos.
+    for video in videos:
+        # Create a list item with a text label and a thumbnail image.
+        list_item = xbmcgui.ListItem(label=video['name'])
+        # Set additional info for the list item.
+        list_item.setInfo('video', {'title': video['name'], 'genre': video['genre']})
+        # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
+        # Here we use the same image for all items for simplicity's sake.
+        # In a real-life plugin you need to set each image accordingly.
+        list_item.setArt({'thumb': video['thumb'], 'icon': video['thumb'], 'fanart': video['thumb']})
+        # Set 'IsPlayable' property to 'true'.
+        # This is mandatory for playable items!
+        list_item.setProperty('IsPlayable', 'true')
+        # Create a URL for a plugin recursive call.
+        # Example: plugin://plugin.video.example/?action=play&video=http://www.vidsplay.com/vids/crab.mp4
+        url = get_url(action='play', video=video['video'])
+        # Add the list item to a virtual Kodi folder.
+        # is_folder = False means that this item won't open any sub-list.
+        is_folder = False
+        # Add our item to the Kodi virtual folder listing.
+        xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
+    xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+    # Finish creating a virtual folder.
+    xbmcplugin.endOfDirectory(_handle)
+
+
+def play_video(path):
+    """
+    Play a video by the provided path.
+
+    :param path: Fully-qualified video URL
+    :type path: str
+    """
+    # Create a playable item with a path to play.
+    play_item = xbmcgui.ListItem(path=path)
+    # Pass the item to the Kodi player.
+    xbmcplugin.setResolvedUrl(_handle, True, listitem=play_item)
+
+
+def router(paramstring):
+    """
+    Router function that calls other functions
+    depending on the provided paramstring
+
+    :param paramstring: URL encoded plugin paramstring
+    :type paramstring: str
+    """
+    # Parse a URL-encoded paramstring to the dictionary of
+    # {<parameter>: <value>} elements
+    params = dict(parse_qsl(paramstring))
+    # Check the parameters passed to the plugin
+    if params:
+        if params['action'] == 'listing':
+            # Display the list of videos in a provided category.
+            list_videos(params['category'])
+        elif params['action'] == 'play':
+            # Play a video from a provided URL.
+            play_video(params['video'])
+        else:
+            # If the provided paramstring does not contain a supported action
+            # we raise an exception. This helps to catch coding errors,
+            # e.g. typos in action names.
+            raise ValueError('Invalid paramstring: {0}!'.format(paramstring))
+    else:
+        # If the plugin is called from Kodi UI without any parameters,
+        # display the list of video categories
+        list_categories()
+
+
 if __name__ == '__main__':
-    sample_page = 'http://kodinerds.bplaced.net/audiobooks/tkkg/'
-    addon_handle = int(sys.argv[1])
-    main()
+    # Call the router function and pass the plugin call parameters to it.
+    # We use string slicing to trim the leading '?' from the plugin call paramstring
+    router(sys.argv[2][1:])
